4202a2d2acfe4c6000c49f9aa2cd5088de590319
vrx <asquirespirelab@gmail.com>
2022-04-22 19:56:12 +0530

    basic setup

--- src/app/App.js
+++ src/app/App.js
@@ -1,32 +1,12 @@
 import { Box } from "@mui/material";
 import * as React from "react";
+import Control from "../components/Control";
 
-const classes = {
-	img: {
-		width: "auto",
-	},
-};
-
-const App = React.memo(function App({ src, children, title }) {
-	const onDrop = React.useCallback(async (files) => {
-		try {
-			const file = files[0];
-			if (!file) return;
-
-			const timestamp = new Date();
-			const src = URL.createObjectURL(file);
-
-			console.log("file", file);
-		} catch (err) {
-			log(err);
-		}
-	}, []);
-
+const App = React.memo(function App() {
 	return (
 		<div>
-			<h1>App</h1>
 			<Box mb={2}>
-				<div>box lol</div>
+				<Control />
 			</Box>
 		</div>
 	);
