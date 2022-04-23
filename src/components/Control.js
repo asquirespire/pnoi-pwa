5d573ad6a8a897523d6157b6403c19361a2e8f0c
vrx <asquirespirelab@gmail.com>
2022-04-23 17:05:56 +0530

    axios requests

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Box } from "@mui/material";
+import axios from "axios";
 
 // Components
 
@@ -11,13 +12,94 @@ import { Button } from "@mui/material";
 import { Typography } from "@mui/material";
 
 export const ControlUI = React.memo(function ControlUI({ nav }) {
+	const handleStart = () => {
+		axios
+			.get("/do/start", { do: "start" })
+			.then((r) => console.log(r, "start request"))
+			.catch((e) => console.log(e));
+	};
+
+	const handleStop = () => {
+		axios
+			.get("/do/stop", { do: "stop" })
+			.then((r) => console.log(r, "stop request"))
+			.catch((e) => console.log(e));
+	};
+
+	const handleTx = () => {
+		axios({
+			url: "/transfer", //your url
+			method: "GET",
+			responseType: "blob", // important
+		})
+			.then((response) => {
+				const data = response.data;
+				console.log({ data });
+				const url = window.URL.createObjectURL(new Blob([data]));
+				const link = document.createElement("a");
+				link.href = url;
+				link.setAttribute("download", "rec.wav"); //or any other extension
+				document.body.appendChild(link);
+				link.click();
+			})
+			.then((link) => {
+				handleDoneDownload(true)
+					.then((res) => {
+						console.log(res);
+					})
+					.catch((e) => console.log(e));
+			})
+			.catch((e) => {
+				handleDoneDownload(false)
+					.then((res) => console.log(res, e))
+					.catch((e) => console.log(e));
+			});
+	};
+
+	const handleDoneDownload = async (state) => {
+		const article = { title: "download state" };
+		const params = { message: state ? "done" : "failed" };
+		const response = await axios.post("/transferack", article, { params });
+		return response;
+	};
 	return (
 		<div className={classes.root}>
 			<Typography className={classes.title} variant="h6">
 				Control
 			</Typography>
 			<Box className={classes.control} mb={2}>
-				<div>basic Control</div>
+				<div className="App">
+					<header className="App-header">
+						<p>ESP32 | Pnoi-phone App</p>
+
+						<div className="App-Buttons-div">
+							<Button
+								className="App-Button"
+								type="button"
+								onClick={handleStart}
+							>
+								Start
+							</Button>
+
+							<Button
+								className="App-Button"
+								type="button"
+								onClick={handleStop}
+							>
+								Stop
+							</Button>
+
+							<Button
+								className="App-Button"
+								type="button"
+								onClick={handleTx}
+								// href="/transfer"
+							>
+								Transfer
+							</Button>
+						</div>
+					</header>
+				</div>
 			</Box>
 		</div>
 	);
